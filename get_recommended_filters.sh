1d4bd5e83a98b31140f750e73dac228dc6fa7685
s0uboxxx <96679268+s0uboxxx@users.noreply.github.com>
2023-08-09 12:53:47 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -13,6 +13,7 @@ urls=(
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://abpvn.com/filter/abpvn-emGOMQ.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
