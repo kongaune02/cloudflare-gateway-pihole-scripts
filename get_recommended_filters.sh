5943f65f3cabb8bc840a95cb6ff6197adbf0a31d
s0uboxxx <96679268+s0uboxxx@users.noreply.github.com>
2023-08-10 16:33:35 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -14,7 +14,7 @@ urls=(
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-  https://abpvn.com/filter/abpvn-emGOMQ.txt
+  # https://abpvn.com/filter/abpvn-emGOMQ.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
